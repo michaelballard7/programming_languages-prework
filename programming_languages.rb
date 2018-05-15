8bf6298426e59b6243362aca41cc9495bfd0aad9
Michael Ballard <ballard.michael9@gmail.com>
2018-05-15 12:47:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,12 +2,12 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = { }
   languages.each do|language_paradigm, language|
-    binding.pry
+    #binding.pry
     language.each do |language_name, type|
     new_hash[language_name] = type
-    binding.pry
-    new_hash[language_name].merge!({style: [language_paradigm]}) 
     #binding.pry
+    new_hash[language_name].merge!({style: [language_paradigm]}) 
+    binding.pry
     end
   end
   new_hash
